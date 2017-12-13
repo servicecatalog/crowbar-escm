a2cea01c3b057ac30d2e36d88efcae6d889436cc
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-12-13 17:19:24 +0100

    Added tenant id param

--- chef/cookbooks/escm/recipes/server.rb
+++ chef/cookbooks/escm/recipes/server.rb
@@ -297,12 +297,22 @@ ruby_block "get_escm_secrets" do
     action :create
 end
 
+ruby_block "get_escm_project_id" do
+    block do
+      Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+      command = "#{openstack_cmd} project show -c id -f value #{escm_project}
+      node[:escm][:keystone][:project][:id] = command_out.stdout.strip
+    end
+    action :create
+end
+
 var_no_proxy = node[:escm][:proxy][:no_proxy].empty? ? "#{node[:escm][:proxy][:no_proxy_default]},#{node[:escm][:openstack][:instance_stack][:ip_appserver]}" : "#{node[:escm][:proxy][:no_proxy_default]},#{node[:escm][:openstack][:instance_stack][:ip_appserver]},#{node[:escm][:proxy][:no_proxy]}"
 var_key_secret = "#{node[:escm][:openstack][:instance_stack][:key_secret]}"
 var_host_fqdn = node[:escm][:ssl][:fqdn].empty? ? "#{node[:escm][:openstack][:instance_stack][:ip_appserver]}" : "#{node[:escm][:ssl][:fqdn]}"
 var_db_pwd_core = "#{node[:escm][:openstack][:instance_stack][:db_core_password]}"
 var_db_pwd_app = "#{node[:escm][:openstack][:instance_stack][:db_app_password]}"
 var_db_superpwd = "#{node[:escm][:openstack][:instance_stack][:db_password]}"
+var_escm_project_id = "#{node[:escm][:keystone][:project][:id]}"
 
 template "#{escm_install_path}/var.env" do
   source "var.env.erb"
@@ -321,7 +331,8 @@ template "#{escm_install_path}/var.env" do
     db_superpwd: var_db_superpwd,
     os_keystone_url: keystone_settings["internal_auth_url"],
     os_user: node[:escm][:keystone][:user],
-	os_password: node[:escm][:keystone][:password]
+	os_password: node[:escm][:keystone][:password],
+	os_tenant_id: var_escm_project_id
   )
 end
 
